3009f270e045681235ee44dadc2024de66ca5934
Flame Nicoo <86409467+flamenicoo@users.noreply.github.com>
2022-01-13 22:05:04 +0400

    Add files via upload

---
+++ rammer.py
@@ -0,0 +1,11 @@
+import psutil
+import time
+print("Ctrl+C to exit.")
+
+def Status():
+    while True:
+        print("RAM percent :",psutil.virtual_memory().percent,"%",end='')
+        print(" | Available RAM :",psutil.virtual_memory().available/1024,end='')
+        print(" | Used RAM :",psutil.virtual_memory().used/1024,end='\r')
+        time.sleep(1)
+Status()
